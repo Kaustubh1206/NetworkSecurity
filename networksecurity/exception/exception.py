93b2f9840fcee68bee7aff90c3a34e52a0986dd9
Kautubh1206 <kaustubhgidh06@gmail.com>
2025-09-23 15:07:37 -0400

    Mongo DB

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -16,3 +16,4 @@ class NetworkSecurityException(Exception):
         return "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
             self.file_name , self.lineno, str(self.error_message))
 
+bv        
