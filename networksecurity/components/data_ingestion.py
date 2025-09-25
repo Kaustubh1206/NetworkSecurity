2889eada345e2c362fd0adaa8be650c8a87b1619
Kautubh1206 <kaustubhgidh06@gmail.com>
2025-09-24 20:15:42 -0400

    data

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -2,3 +2,6 @@ import os
 import sys 
 
 from dataclasses import dataclass
+
+from networksecurity.exception import exception
+ 
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -18,21 +18,36 @@ class TrainingPipelineConfig:
 
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        # Training Pipeline
         self.data_ingestion=os.path.join(
             training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME
         )
+
+        # Training Pipeline Directory  artifacts
         self.data_ingestion_dir:str=os.path.join(
             training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME
         )
+
+        #
         self.feature_store_file_path: str = os.path.join(
                 self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
             )
+        
+        # Training path name
         self.training_file_path: str = os.path.join(
                 self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME
             )
+        
+        # Testing path name 
         self.testing_file_path: str = os.path.join(
                 self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME
             )
+        
+        # Train Tes SPlit ratio 
         self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
+
+        # Data ingestion collection name
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
-        self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAM
+
+        # Database name
+        self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME 
